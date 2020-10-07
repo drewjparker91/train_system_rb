6caea2654d29bac5c90f6e4bb6350ec609801bde
drewparker <drewjparker91@gmail.com>
2020-10-07 11:00:24 -0700

    create test for #save  with good fail and pass Co-Authored-By: Adam Mansell <adammansellhmb@gmail.com> Co-Authored-By: David McCaslin <davidmccaslin94@gmail.com>

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 
 describe 'Doctor' do
   
-  describe ('.all') do
+  describe('.all') do
     it("returns an empty array when there are no do trains") do
       expect(Train.all).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it("saves a train") do
+      train = Train.new({:name => "Red Train", :id => nil})
+      train.save()
+      train2 = Train.new({:name => "Blue Train", :id => nil})
+      train2.save()
+      expect(Train.all).to(eq([train, train2]))
+    end
+  end
+
 end
